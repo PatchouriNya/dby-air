e63a70e339e3900b8cab9ffde6fcdd38b6c5223c
patchouri <609206398@qq.com>
2024-05-09 16:29:54 +0800

    修改策略表结构,准备策略也使用树实现

--- api/database/migrations/2024_04_28_091812_create_strategies_table.php
+++ api/database/migrations/2024_04_28_091812_create_strategies_table.php
@@ -15,6 +15,7 @@ class CreateStrategiesTable extends Migration
     {
         Schema::create('strategies', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('client_id')->comment('所属客户ID');
             $table->string('name');
             $table->text('info')->comment('策略概述');
             $table->string('power_state', 255)->comment('开机状态');
@@ -23,14 +24,15 @@ class CreateStrategiesTable extends Migration
             $table->string('wind_mode', 255)->comment('风向模式');
             $table->string('set_temperature', 255)->comment('设置温度');
             $table->string('electrify_state', 255)->nullable()->comment('通电状态');
-            $table->timestamp('start_date')->comment('开始日期');
-            $table->timestamp('end_date')->comment('结束日期');
+            $table->timestamp('start_date')->nullable()->comment('开始日期');
+            $table->timestamp('end_date')->nullable()->comment('结束日期');
             $table->string('start_time', 30)->comment('开始时间');
             $table->string('end_time', 30)->comment('结束时间');
             $table->tinyInteger('interval_time')->default(5)->comment('执行间隔时间,单位分钟');
             $table->string('week_days', 255)->comment('运行周期');
             $table->tinyInteger('status')->default(0)->comment('是否在运行1:在运行 0:停止');
             $table->timestamps();
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
         });
     }
 

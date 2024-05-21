4d65a6aef0e9d9d9fe7c7b6a03f4fa54da7d4a57
patchouri <609206398@qq.com>
2024-05-21 14:03:09 +0800

    定时执行策略脚本修改(现存在一个需求之外的bug,若多个策略间隔时间不一样只会执行最短间隔的所有策略)

--- api/app/Console/Commands/ExecuteStrategies.php
+++ api/app/Console/Commands/ExecuteStrategies.php
@@ -48,50 +48,59 @@ class ExecuteStrategies extends Command
         $week_day = $currentTime->dayOfWeek;
         $groups = Air_group::whereNotNull('strategy_id')->get();
         foreach ($groups as $group) {
-            $strategy = Strategy::find($group->strategy_id);
+            //            $strategy = Strategy::find($group->strategy_id);
+            $strategy_arr = $group->strategy_id;
             // 计算策略上次执行时间
             $lastExecutedAt = strtotime($group->updated_at);
-            $interval = $strategy->interval_time * 60; // 转换为秒
-            // 判断是否需要执行策略
-            if ($currentTime->timestamp - $lastExecutedAt >= $interval) {
-                if (
-                    ($strategy->start_time <= $time && $strategy->end_time >= $time)
-                    && ($strategy->start_date <= $date && $strategy->end_date >= $date)
-                    && in_array($week_day, $strategy->week_days)) {
-                    $delta = $strategy->delta;
-                    $airIds = Air_group_relationship::where('group_id', $group->id)->pluck('air_id');
-                    if ($delta === 0) {
-                        Air_detail::whereIn('id', $airIds)
-                            ->update([
-                                'power_state'     => $strategy->power_state,
-                                'operation_mode'  => $strategy->operation_mode,
-                                'wind_speed'      => $strategy->wind_speed,
-                                'wind_mode'       => $strategy->wind_mode,
-                                'set_temperature' => $strategy->set_temperature,
-                            ]);
+            foreach ($strategy_arr as $strategy_id) {
+                $strategy = Strategy::find($strategy_id);
 
-                    } else {
-                        foreach ($airIds as $airId) {
-                            $air = Air_detail::find($airId);
-                            $temperature = (int)$air->set_temperature;
-                            if (!(($temperature <= (int)$strategy->set_temperature + $delta) && ($temperature >= (int)$strategy->set_temperature - $delta))) {
-                                $temperature = (int)$strategy->set_temperature;
+                $interval = $strategy->interval_time * 60; // 转换为秒
+                // 判断是否需要执行策略
+                if ($currentTime->timestamp - $lastExecutedAt >= $interval) {
+                    if (
+                        ($strategy->start_time <= $time && $strategy->end_time >= $time)
+                        && ($strategy->start_date <= $date && $strategy->end_date >= $date)
+                        && in_array($week_day, $strategy->week_days)) {
+                        $delta = $strategy->delta;
+                        $airIds = Air_group_relationship::where('group_id', $group->id)->pluck('air_id');
+                        if ($delta === 0) {
+                            Air_detail::where('power_state', '开机')->whereIn('id', $airIds)
+                                ->update([
+                                    'power_state'     => $strategy->power_state,
+                                    'operation_mode'  => $strategy->operation_mode,
+                                    'wind_speed'      => $strategy->wind_speed,
+                                    'wind_mode'       => $strategy->wind_mode,
+                                    'set_temperature' => $strategy->set_temperature,
+                                ]);
+
+                        } else {
+                            foreach ($airIds as $airId) {
+                                $air = Air_detail::find($airId);
+                                if ($air->power_state === '开机') {
+
+                                    $temperature = (int)$air->set_temperature;
+                                    if (!(($temperature <= (int)$strategy->set_temperature + $delta) && ($temperature >= (int)$strategy->set_temperature - $delta))) {
+                                        $temperature = (int)$strategy->set_temperature;
+                                    }
+                                    $air->update([
+                                        'power_state'     => $strategy->power_state,
+                                        'operation_mode'  => $strategy->operation_mode,
+                                        'wind_speed'      => $strategy->wind_speed,
+                                        'wind_mode'       => $strategy->wind_mode,
+                                        'set_temperature' => $temperature . '℃'
+                                    ]);
+                                }
                             }
-                            $air->update([
-                                'power_state'     => $strategy->power_state,
-                                'operation_mode'  => $strategy->operation_mode,
-                                'wind_speed'      => $strategy->wind_speed,
-                                'wind_mode'       => $strategy->wind_mode,
-                                'set_temperature' => $temperature . '℃'
-                            ]);
                         }
+                        $group->updated_at = now();
+                        $group->save();
+                        $clientname = Client::find($group->client_id)->clientname;
+                        $this->info(now()->format('Y-m-d H:i:s') . ' ' . $clientname . ' 的空调组 ' . $group->name . ' 执行了策略 ' . $strategy->name);
                     }
-                    $group->updated_at = now();
-                    $group->save();
-                    $clientname = Client::find($group->client_id)->clientname;
-                    $this->info(now()->format('Y-m-d H:i:s') . ' ' . $clientname . ' 的空调组 ' . $group->name . ' 执行了策略 ' . $strategy->name);
                 }
             }
+
         }
         return 1;
     }

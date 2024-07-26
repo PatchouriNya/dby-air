4f59e16165b99a26e069ce0921d5a586bfc9fd7e
patchouri <609206398@qq.com>
2024-07-27 00:48:04 +0800

    为客户表新增字段以便于代码复用

--- api/database/migrations/2024_03_13_211701_create_clients_table.php
+++ api/database/migrations/2024_03_13_211701_create_clients_table.php
@@ -15,12 +15,16 @@ class CreateClientsTable extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('clientname',50)->unique()->comment('客户公司名称');
-            $table->string('province',10)->comment('省');
-            $table->string('city',10)->comment('城');
-            $table->string('district',10)->comment('区');
-            $table->bigInteger('pid',false)->default(0)->comment('父级id,默认为0代表顶级');
-            $table->tinyInteger('level',false)->default(1)->comment('0为目录，1为可点击');
+            $table->string('clientname', 50)->unique()->comment('客户公司名称');
+            $table->string('province', 10)->comment('省');
+            $table->string('city', 10)->comment('城');
+            $table->string('district', 10)->comment('区');
+            $table->bigInteger('pid', false)->default(0)->comment('父级id,默认为0代表顶级');
+            $table->tinyInteger('level', false)->default(1)->comment('0为目录，1为可点击');
+            $table->string('host_address', 20)->nullable()->comment('主机地址');
+            $table->string('com_port', 10)->nullable()->comment('串口');
+            $table->string('start_address', 20)->nullable()->comment('16进制起始地址');
+            $table->integer('total_air', false)->nullable()->comment('总空调数量');
             $table->timestamps();
             $table->softDeletes();
         });

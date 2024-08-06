caafb5b1efc8ce6e90142ac5b15706b1d710d50a
patchouri <609206398@qq.com>
2024-08-06 16:41:53 +0800

    新增读写地址字段

--- api/database/migrations/2024_03_20_165712_create_air_details_table.php
+++ api/database/migrations/2024_03_20_165712_create_air_details_table.php
@@ -15,25 +15,27 @@ class CreateAirDetailsTable extends Migration
     {
         Schema::create('air_details', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('client_id',false)->comment('客户公司id,用于关联表');
-            $table->integer('show_id',false)->comment('表格中展示的id');
-            $table->string('designation',255)->nullable()->comment('名称');
-            $table->string('responsible_person',255)->nullable()->comment('责任人');
-            $table->string('air_brand',255)->nullable()->comment('品牌');
-            $table->string('online_state',255)->nullable()->comment('在线状态');
-            $table->string('electrify_state',255)->nullable()->comment('通电状态');
-            $table->string('power_state',255)->nullable()->comment('开机状态');
-            $table->string('operation_mode',255)->nullable()->comment('运行模式');
-            $table->string('wind_speed',255)->nullable()->comment('风速');
-            $table->string('wind_mode',255)->nullable()->comment('风向模式');
-            $table->string('set_temperature',255)->nullable()->comment('设置温度');
-            $table->string('room_temperature',255)->nullable()->comment('室温');
-            $table->string('voltage',255)->nullable()->comment('电压');
-            $table->string('electric_current',255)->nullable()->comment('电流');
-            $table->string('power',255)->nullable()->comment('功率');
-            $table->string('electric_quantity',255)->nullable()->comment('电量读取');
-            $table->tinyInteger('standby_mode',false)->nullable()->comment('待机状态');
-            $table->unsignedTinyInteger('is_grouped',false)->default(0)->comment('是否已被分组,是为1,否为0');
+            $table->bigInteger('client_id', false)->comment('客户公司id,用于关联表');
+            $table->integer('show_id', false)->comment('表格中展示的id');
+            $table->string('read_base_address', 20)->nullable()->comment('读取时寄存器起始地址');
+            $table->string('write_base_address', 20)->nullable()->comment('写入时寄存器起始地址');
+            $table->string('designation', 255)->nullable()->comment('名称');
+            $table->string('responsible_person', 255)->nullable()->comment('责任人');
+            $table->string('air_brand', 255)->nullable()->comment('品牌');
+            $table->string('online_state', 255)->nullable()->comment('在线状态');
+            $table->string('electrify_state', 255)->nullable()->comment('通电状态');
+            $table->string('power_state', 255)->nullable()->comment('开机状态');
+            $table->string('operation_mode', 255)->nullable()->comment('运行模式');
+            $table->string('wind_speed', 255)->nullable()->comment('风速');
+            $table->string('wind_mode', 255)->nullable()->comment('风向模式');
+            $table->string('set_temperature', 255)->nullable()->comment('设置温度');
+            $table->string('room_temperature', 255)->nullable()->comment('室温');
+            $table->string('voltage', 255)->nullable()->comment('电压');
+            $table->string('electric_current', 255)->nullable()->comment('电流');
+            $table->string('power', 255)->nullable()->comment('功率');
+            $table->string('electric_quantity', 255)->nullable()->comment('电量读取');
+            $table->tinyInteger('standby_mode', false)->nullable()->comment('待机状态');
+            $table->unsignedTinyInteger('is_grouped', false)->default(0)->comment('是否已被分组,是为1,否为0');
 
             $table->timestamps();
             $table->softDeletes();
